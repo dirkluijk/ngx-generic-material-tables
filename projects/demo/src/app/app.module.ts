49a9da266c61d3d9b6cf38846e2e72198565de36
Dirk Luijk <mail@dirkluijk.nl>
2019-08-16 15:51:17 +0200

    build: rename library
    BREAKING CHANGE: changed the npm package name

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material';
-import { SortModule } from '@dirkluijk/generic-material-tables';
+import { SortModule } from '@dirkluijk/ngx-generic-material-tables';
 
 import { AppComponent } from './app.component';
 
